c2126fcdcca6b49e5ba6f9db9c1cfe5a5ba54a83
zpty still <zpty@outlook.com>
2018-01-22 18:38:51 +0800

    try use softwareBitmap

--- 视频缩略图/MainPage.xaml.cs
+++ 视频缩略图/MainPage.xaml.cs
@@ -6,11 +6,13 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
 using Windows.Storage;
 using Windows.Storage.FileProperties;
 using Windows.Storage.Pickers;
 using Windows.Storage.Streams;
 using Windows.UI.Popups;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -34,6 +36,9 @@ namespace 视频缩略图
         public MainPage()
         {
             this.InitializeComponent();
+            // 
+            ApplicationView.PreferredLaunchViewSize = new Size(400, 200);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
         }
 
         private async void Button_Click(object sender, RoutedEventArgs e)
@@ -49,11 +54,12 @@ namespace 视频缩略图
             picker.FileTypeFilter.Add(".avi");
             picker.FileTypeFilter.Add(".wmv");
 
+            Message.Text = "正在选择视频";
             files = await picker.PickMultipleFilesAsync();
             if (files != null)
             {
                 isSelected = true;
-           
+                Message.Text = "已选择视频";
             }
         }
 
@@ -73,17 +79,17 @@ namespace 视频缩略图
                 foreach (var file in files)
                 {
                     // 获取视频截图
-                    var objThumbnail = await file.GetScaledImageAsThumbnailAsync(ThumbnailMode.VideosView,400,ThumbnailOptions.ResizeThumbnail);
+                    var objThumbnail = await file.GetScaledImageAsThumbnailAsync(ThumbnailMode.VideosView);
                     // 创建buffer
                     var buffer = new Windows.Storage.Streams.Buffer(Convert.ToUInt32(objThumbnail.Size));
                     // 转为buffer
                     var buffer1 = await objThumbnail.ReadAsync(buffer, buffer.Capacity, InputStreamOptions.None);
 
+                    SoftwareBitmap outputBitmap = SoftwareBitmap.CreateCopyFromBuffer(buffer1, BitmapPixelFormat.Bgra8, 400, 300);
                     // 获取保存文件对象
                     var saveFile = await storageFolder.CreateFileAsync(file.DisplayName + ".jpg");
-
-                    // 将buffer写入头文件
-                    await FileIO.WriteBufferAsync(saveFile, buffer1);
+                    // 写入文件
+                    SaveSoftwareBitmapToFile(outputBitmap, saveFile);
                 }
                 dialog = new MessageDialog("处理完成");
                 dialog.Commands.Add(new UICommand("确定", cmd => { }, commandId: 0));
@@ -100,6 +106,56 @@ namespace 视频缩略图
                 //获取返回值
                 var result = await dialog.ShowAsync();
 
+            }
+        }
+
+
+        /// <summary>
+        /// 保存softwarebitmap到文件
+        /// </summary>
+        /// <param name="softwareBitmap"></param>
+        /// <param name="outputFile"></param>
+        private async void SaveSoftwareBitmapToFile(SoftwareBitmap softwareBitmap, StorageFile outputFile)
+        {
+            using (IRandomAccessStream stream = await outputFile.OpenAsync(FileAccessMode.ReadWrite))
+            {
+                // 设置Encoder属性,jpeg 质量
+                var propertySet = new BitmapPropertySet();
+                var qualityValue = new BitmapTypedValue(1.0, PropertyType.Single);
+                propertySet.Add("ImageQuality", qualityValue);
+
+                BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, stream, propertySet);
+                encoder.SetSoftwareBitmap(softwareBitmap);
+
+                // 设置输出文件属性
+                encoder.BitmapTransform.ScaledWidth = 320;
+                encoder.BitmapTransform.ScaledHeight = 240;
+                // encoder.BitmapTransform.Rotation = Windows.Graphics.Imaging.BitmapRotation.Clockwise90Degrees;
+                // 设置插值方式
+                encoder.BitmapTransform.InterpolationMode = BitmapInterpolationMode.Linear;
+                encoder.IsThumbnailGenerated = true;
+
+                try
+                {
+                    await encoder.FlushAsync();
+                }
+                catch (Exception err)
+                {
+                    switch (err.HResult)
+                    {
+                        case unchecked((int)0x88982F81): //WINCODEC_ERR_UNSUPPORTEDOPERATION
+                                                         // If the encoder does not support writing a thumbnail, then try again
+                                                         // but disable thumbnail generation.
+                            encoder.IsThumbnailGenerated = false;
+                            break;
+                        default:
+                            throw err;
+                    }
+                }
+                if (encoder.IsThumbnailGenerated == false)
+                {
+                    await encoder.FlushAsync();
+                }
             }
         }
     }

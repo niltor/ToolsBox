26ac21d775f505bb618216288380a0372a3e528d
zpty still <zpty@outlook.com>
2018-05-24 14:33:29 +0800

    update  .sln

--- PostmanToMD/Models/ApiModel.cs
+++ PostmanToMD/Models/ApiModel.cs
@@ -1,10 +1,10 @@
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace PostmanToMD.Models
 {
@@ -31,6 +31,7 @@ namespace PostmanToMD.Models
 
         public ApiModel()
         {
+
         }
 
         /// <summary>

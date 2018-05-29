a1a2a4f2d51ff52c4288e9cd8619f371d60f1cdd
zpty still <zpty@outlook.com>
2018-05-29 18:26:57 +0800

    parse request && response

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
 
@@ -18,9 +19,61 @@ namespace Test
                 MetadataPropertyHandling = MetadataPropertyHandling.Ignore
 
             };
-            var swagger = JsonConvert.DeserializeObject<SwaggerDocument>(content,setting
-                );
+            var swagger = JsonConvert.DeserializeObject<SwaggerDocument>(content, setting);
+
+            var apiModel = new SwaggerApiModel
+            {
+                Author = swagger.info.contact?.name,
+                Email = swagger.info.contact?.email,
+                Introduction = swagger.info.description,
+                Name = swagger.info.title,
+                Definitions = swagger.definitions
+            };
+
+            var items = new List<SwaggerApiItem>();
+            // 枚举有效方法
+            foreach (var path in swagger.paths)
+            {
+                var pathItem = ToDictionary(path.Value);
+                foreach (var item in pathItem)
+                {
+                    if (item.Value is Operation value)
+                    {
+                        var apiItem = new SwaggerApiItem
+                        {
+                            Folder = path.Key,
+                            Method = item.Key,
+                            Description = value.description,
+                            Name = value.summary,
+                            Url = path.Key,
+                            RequestBodyType = value.parameters?[0]?.@in,
+                            Request = value.parameters,
+                            Response = value.responses,
+                            OperationId = value.operationId
+                        };
+
+                        items.Add(apiItem);
+                    }
+                    continue;
+                }
+            }
+            var result=apiModel.GetItemsContent(items);
+            File.WriteAllText("outputl.md", result);
+
             Console.ReadLine();
         }
+
+        static Dictionary<string, object> ToDictionary(Object obj)
+        {
+            var result = new Dictionary<string, object>();
+            var fields = obj.GetType().GetFields();
+
+            foreach (var field in fields)
+            {
+                result.Add(field.Name, field.GetValue(obj));
+            }
+            return result;
+        }
+
     }
 }

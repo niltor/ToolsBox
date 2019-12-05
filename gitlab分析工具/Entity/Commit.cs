c73f4ff33a93bd40f517e7444bfcd9337ce43ebe
niltor <zpty@outlook.com>
2019-12-05 16:12:38 +0800

    gitlab统计commit

--- gitlab分析工具/Entity/Commit.cs
+++ gitlab分析工具/Entity/Commit.cs
@@ -6,12 +6,29 @@ namespace gitlab分析工具.Entity
 {
     public class Commit
     {
+        /// <summary>
+        /// 姓名
+        /// </summary>
         public string UserName { get; set; }
+        /// <summary>
+        /// 日期
+        /// </summary>
         public DateTime CreatedTime { get; set; }
+        /// <summary>
+        /// 项目名称
+        /// </summary>
         public string ProjectName { get; set; }
-
+        /// <summary>
+        /// 添加
+        /// </summary>
         public int Additions { get; set; }
+        /// <summary>
+        /// 删除
+        /// </summary>
         public int Deletions { get; set; }
+        /// <summary>
+        /// 变动
+        /// </summary>
         public int Total { get; set; }
 
     }
--- gitlab分析工具/MainWindow.xaml.cs
+++ gitlab分析工具/MainWindow.xaml.cs
@@ -1,20 +1,15 @@
-﻿using gitlab分析工具.Models;
+﻿using CsvHelper;
+using gitlab分析工具.Models;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace gitlab分析工具
 {
@@ -25,12 +20,13 @@ namespace gitlab分析工具
     {
         string projectUrl = "/api/v4/projects";
         string commitUrl = "/api/v4/projects/:id/repository/commits?all=true&with_stats=true";
-        string commitDetailUrl = "/api/v4/projects/:id/repository/commits/:sha";
         HttpClient hc = new HttpClient();
+        List<Entity.Commit> userCommit = new List<Entity.Commit>();
 
         public MainWindow()
         {
             InitializeComponent();
+            SaveToCsvBtn.IsEnabled = false;
         }
 
         private async void GetDataBtn_ClickAsync(object sender, RoutedEventArgs e)
@@ -48,32 +44,35 @@ namespace gitlab分析工具
             GetDataBtn.IsEnabled = false;
             // 获取项目
             var projects = await GetProjectsAsync();
+            projects = projects.OrderBy(p => p.id).ToList();
 
             // 获取提交
-            var userCommit = new List<Entity.Commit>();
             foreach (var project in projects)
             {
                 var commit = await GetCommits(project);
                 // 处理数据格式
-                var currentCommit = commit.Select(s => new Entity.Commit
+                if (commit != null)
                 {
-                    Additions = s.stats.additions,
-                    CreatedTime = s.created_at,
-                    Deletions = s.stats.deletions,
-                    ProjectName = project.name,
-                    Total= s.stats.total,
-                    UserName= s.committer_name,
+                    var currentCommit = commit.Select(s => new Entity.Commit
+                    {
+                        Additions = s.stats.additions,
+                        CreatedTime = s.created_at,
+                        Deletions = s.stats.deletions,
+                        ProjectName = project.name,
+                        Total = s.stats.total,
+                        UserName = s.committer_name,
 
-                }).ToList();
-                userCommit.AddRange(currentCommit);
-                RunMessageTB.Text += "项目" + project.name + " => 共 " + userCommit.Count + "\r\n";
-                RunMessageTB.ScrollToEnd();
+                    }).ToList();
+                    userCommit.AddRange(currentCommit);
+                    RunMessageTB.Text += "项目" + project.name + "[" + project.id + "] => 共 " + userCommit.Count + "\r\n";
+                    RunMessageTB.ScrollToEnd();
+                }
             }
-
-            // TODO:保存数据
-       
+            RunMessageTB.Text += "已全部采集完成\r\n";
+            RunMessageTB.ScrollToEnd();
 
             GetDataBtn.IsEnabled = true;
+            SaveToCsvBtn.IsEnabled = true;
         }
 
 
@@ -85,6 +84,7 @@ namespace gitlab分析工具
         {
             var url = ServerUrl.Text + projectUrl + "?per_page=100";
             hc.DefaultRequestHeaders.TryAddWithoutValidation("PRIVATE-TOKEN", PAT.Text);
+            hc.Timeout = TimeSpan.FromSeconds(30);
             var response = await hc.GetStringAsync(url);
             var projects = JsonSerializer.Deserialize<List<Project>>(response);
             return projects;
@@ -98,27 +98,64 @@ namespace gitlab分析工具
         {
             var result = new List<Commit>();
             var url = ServerUrl.Text + commitUrl.Replace(":id", project.id.ToString());
-            var response = await hc.GetAsync(url);
-            if (!response.IsSuccessStatusCode)
-            {
-                MessageBox.Show("获取commit失败,projectId:" + project.id);
-                return default;
-            }
-            // 取出分页信息
-            var total = response.Headers.Where(h => h.Key == "X-Total")
-                .SingleOrDefault().Value?.FirstOrDefault();
-            int pageSize = 100;
-            var page = Convert.ToInt32(total) / pageSize + 1;
+            int errorPage = 0;
 
-            for (int i = 1; i <= page; i++)
+            try
             {
-                var pageUrl = url + "&page=" + i + "&per_page=" + pageSize;
-                var _ = await hc.GetStringAsync(url);
-                var commits = JsonSerializer.Deserialize<List<Commit>>(_);
-                result.AddRange(commits);
+                var response = await hc.GetAsync(url);
+                if (!response.IsSuccessStatusCode)
+                {
+                    RunMessageTB.Text += "==== 获取commit失败,projectId:" + project.id + "\r\n";
+                    return default;
+                }
+                // 取出分页信息
+                var total = response.Headers.Where(h => h.Key == "X-Total")
+                    .SingleOrDefault().Value?.FirstOrDefault();
+                int pageSize = 100;
+                var page = Convert.ToInt32(total) / pageSize + 1;
+
+                for (int i = 1; i <= page; i++)
+                {
+                    errorPage = i;
+                    var pageUrl = url + "&page=" + i + "&per_page=" + pageSize;
+                    var _ = await hc.GetStringAsync(pageUrl);
+                    var commits = JsonSerializer.Deserialize<List<Commit>>(_);
+                    result.AddRange(commits);
+                }
             }
+            catch (Exception)
+            {
+                RunMessageTB.Text += "==== PID:" + project.id + ";page=" + errorPage + "\r\n";
+                Console.WriteLine("projectId: " + project.id);
+            }
+
+            Thread.Sleep(100);
             return result;
         }
 
+        private void SaveToCsvBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new SaveFileDialog
+            {
+                DefaultExt = ".csv",
+                Filter = "csv file(*.csv)|*.csv",
+                FileName = "data"
+            };
+
+            if (dlg.ShowDialog() == true)
+            {
+                SaveToCsvBtn.IsEnabled = false;
+                // 保存数据
+                using (var writer = new StreamWriter(dlg.FileName))
+                {
+                    using (var csv = new CsvWriter(writer))
+                    {
+                        csv.WriteRecords(userCommit);
+                    }
+                }
+                MessageBox.Show("保存成功");
+                SaveToCsvBtn.IsEnabled = true;
+            }
+        }
     }
 }
